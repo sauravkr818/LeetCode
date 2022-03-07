1db7d4a4e7520b77f13355c428683d9768c87783
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-03-08 04:24:28 +0530

    Time: 44 ms (99.54%), Space: 48.4 MB (30.09%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
+        // edge cases
         if(nums[0] == 0 && n>1) return false;
         else if(nums[0] == 0 && n == 1) return true;
         
