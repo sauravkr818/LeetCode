0d7358cf7692a0f74e2d6f71f6659fecda9a80af
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-03-08 20:11:20 +0530

    Time: 26 ms (14.00%), Space: 8 MB (58.87%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -10,6 +10,9 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         ListNode *slow = head, *fast = head;
+        // use the concept of slow and fast pointer;
+        // We use the fact that if there is a cycle exist then slow and fast pointer will definitely meet at somewhere;
+        // if there is no cycle then they will not meet and fast will reach to null
         if(head == NULL) return false;
         while(true){
             if(slow->next == NULL) return false;
