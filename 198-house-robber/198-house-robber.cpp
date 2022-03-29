f477f545bd632455a1015371c782ce24f1349fb4
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-03-29 11:51:23 +0530

    Time: 2 ms (54.20%), Space: 7.7 MB (76.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    int f(int n, vector<int>&nums, vector<int>&dp){
-        if(n == 0) return nums[n];
-        if(n<0) return 0;
-        if(dp[n] != -1) return dp[n];
-        int pick = nums[n] + f(n-2,nums,dp);
-        int not_pick = 0 + f(n-1,nums,dp);
-        return dp[n] = max(pick,not_pick);
-    }
+    // Memoization + recursion
+    
+    // int f(int n, vector<int>&nums, vector<int>&dp){
+    //     if(n == 0) return nums[n];
+    //     if(n<0) return 0;
+    //     if(dp[n] != -1) return dp[n];
+    //     int pick = nums[n] + f(n-2,nums,dp);
+    //     int not_pick = 0 + f(n-1,nums,dp);
+    //     return dp[n] = max(pick,not_pick);
+    // }
     
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,-1);
-        return f(n-1,nums,dp);
+        vector<int>dp(n,0);
+        // return f(n-1,nums,dp);
+        dp[0] = nums[0];
+        for(int i=1;i<n;i++){
+            // take
+            int take = nums[i];
+            if(i>1){
+                take += dp[i-2];
+            }
+            //not_take
+            int not_take = 0 + dp[i-1];
+            
+            // storing overlapping subproblems
+            dp[i] = max(take,not_take);
+        }
+        return dp[n-1];
     }
 };
