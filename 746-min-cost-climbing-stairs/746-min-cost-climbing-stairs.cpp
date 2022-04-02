7a5883067c49a18d0882205827b495d16709b5a6
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-04-02 13:39:41 +0530

    Time: 11 ms (44.11%), Space: 13.7 MB (51.04%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    // recursive function
     int f(int n, vector<int>&cost, vector<int>&dp){
         if( n < 0) return 0;
         if(n == 0) return cost[0];
@@ -10,10 +11,20 @@ public:
         
         return dp[n] = cost[n] + min(f(n-1,cost,dp),f(n-2,cost,dp));
     }
+    
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         // memoization
+        // vector<int>dp(n,-1);
+        // return min(f(n-1,cost,dp),f(n-2,cost,dp));
+        
+        // tabulation
         vector<int>dp(n,-1);
-        return min(f(n-1,cost,dp),f(n-2,cost,dp));
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2;i<n;i++){
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
+        }
+        return min(dp[n-1],dp[n-2]);
     }
 };
