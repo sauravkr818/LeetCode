3f184140d797e751c602c946388171f7fea63042
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-04-05 09:05:31 +0530

    Time: 167 ms (16.31%), Space: 59.1 MB (38.72%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int maxArea(vector<int>& arr) {
-        int n = arr.size();
-        int max1 = arr[0];
-        int max2 = arr[n-1];
-        int area = 0;
-        int x = 0;
-        int y = n-1;
-        for(int i=0;i<n&&x<y;i++){
-            area = max(area, min(max1,max2)*(abs(y-x)));
-            if(arr[x]<arr[y]){
-                x++;
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int st = 0;
+        int en = n-1;
+        int area = INT_MIN;
+        while(st<=en){
+            int max1 = height[st];
+            int max2 = height[en];
+            
+            area = max(min(max1,max2)*(en-st),area);
+            if(max1<max2){
+                st++;
             }
             else{
-                y--;
+                en--;
             }
-            max1 = arr[x];
-            max2 = arr[y];
-            //max2 = max(max2,arr[n-i-1]);
         }
         return area;
     }
