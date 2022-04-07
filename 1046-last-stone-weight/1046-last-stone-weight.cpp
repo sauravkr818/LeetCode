cf7731da0c854b80f326ae5d2264611d2c4bf7c1
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-04-07 11:47:15 +0530

    Time: 5 ms (18.19%), Space: 7.6 MB (78.96%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -5,23 +5,15 @@ public:
         for(int it: stones){
             pq.push(it);
         }
-        int top = 0;
         while(pq.size() != 1){
             int a = pq.top(); pq.pop();
-            // if(pq.size() == 1) break;
             int b = pq.top(); pq.pop();
-            // if(pq.size() == 1) break;
             if(a == b) {
-                top = a;
                 if(pq.size() == 0) break;
                 continue;
             }
             else if(a>b){
                 pq.push(a-b);
-            }
-            else{
-                cout<<"skip"<<endl;
-                pq.push(b-a);
             }
         }
         if(pq.size() == 0) return 0;
