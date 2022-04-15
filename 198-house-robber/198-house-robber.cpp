cf4a913faa794d8868cd2045ffa51f1b813a0e9e
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-04-15 23:34:12 +0530

    Time: 3 ms (51.98%), Space: 7.9 MB (25.23%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    // Method - 1 = Recursion
+    // Method - 2 = Memoization (Top-down) + Recursion
+    // Method - 3 = Tabulation (Bottom-up)
+    // Method - 4 = Tabulation + Space Optimisation
+    
     // Memoization + recursion
     // T.C. = O(n) and S.C. = O(n) + O(n)
     // int f(int n, vector<int>&nums, vector<int>&dp){
@@ -11,9 +16,9 @@ public:
     //     return dp[n] = max(pick,not_pick);
     // }
     
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>dp(n,0);
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int>dp(n,0);
         // recursion fn call
         // return f(n-1,nums,dp);
         
@@ -35,24 +40,52 @@ public:
 //         }
 //         return dp[n-1];
         
-        int prev,prev2;
-        prev = 0;
-        prev2 = 0;
-        for(int i=0;i<n;i++){
-            // take
-            int take = nums[i];
-            if(i>1){
-                take += prev2;
-            }
-            //not_take
-            int not_take = 0 + prev;
+        
+        // Tabulation + Space Optimisation
+        // T.C. = O(n) and S.C. = O(1)
+        
+//         int prev,prev2;
+//         prev = 0;
+//         prev2 = 0;
+//         for(int i=0;i<n;i++){
+//             // take
+//             int take = nums[i];
+//             if(i>1){
+//                 take += prev2;
+//             }
+//             //not_take
+//             int not_take = 0 + prev;
             
-            // storing overlapping subproblems
-            int curri = max(take,not_take);
+//             // storing overlapping subproblems
+//             int curri = max(take,not_take);
             
-            prev2 = prev;
-            prev = curri;
-        }
-        return prev;
+//             prev2 = prev;
+//             prev = curri;
+//         }
+//         return prev;
+//     }
+    
+    
+    int f(int i, vector<int>&nums, vector<int>&dp){
+        // base cases
+        
+        if(i<0) return 0;
+        
+        if(i == 0) return nums[0];
+        
+        // take and not-take
+        
+        if(dp[i] != -1) return dp[i];
+         
+        int take = nums[i] + f(i-2,nums,dp);
+        
+        int not_take = f(i-1,nums,dp);
+        
+        return dp[i] = max(take,not_take);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n,-1);
+        return f(n-1,nums,dp);
     }
 };
