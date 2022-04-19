dfa28fd0dea201615957ed9dd2641999bab97772
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-04-19 13:48:49 +0530

    Time: 71 ms (12.43%), Space: 57.8 MB (91.32%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -11,49 +11,22 @@
  */
 class Solution {
 public:
-    
-    void f2(TreeNode* root,int i, int j, vector<int>&arr,TreeNode* &first, TreeNode* &second){
-        if(first != NULL && second != NULL) return;
+    void f(TreeNode* root,TreeNode* &prev,TreeNode* &first,TreeNode* &second){
         if(root == NULL) return;
-        if(root-> val == arr[i]) first = root;
-        if(root-> val == arr[j]) second = root;
-        f2(root->left,i,j,arr,first,second);
-        arr.push_back(root->val);
-        f2(root->right,i,j,arr,first,second);
-    }
-    
-    void f(TreeNode* root,vector<int>&arr){
-        if(root == NULL) return;
-        f(root->left,arr);
-        //cout<<root->val<<endl;
-        arr.push_back(root->val);
-        f(root->right,arr);
-    }
-    void recoverTree(TreeNode* root) {
-        TreeNode* one = root;
-        vector<int>arr;
-        f(root,arr);
-        int i;
-        for(i=1;i<arr.size();i++){
-            if(arr[i-1]>arr[i]){
-                break;
+        f(root->left,prev,first,second);
+        if(prev != NULL && root->val<prev->val){
+            if(first == NULL){
+                first = prev;
             }
+            second = root;
         }
-        //cout<<arr[i-1]<<" ";
-        int j = i;
-        while(j<arr.size() && arr[i-1]>arr[j]){
-            //cout<<" j : "<<j<<endl;
-            j++;
-            //cout<<" j : "<<j<<endl;
-        }
-        //cout<<arr[j];
-        root = one;
-        TreeNode* first = NULL;
-        TreeNode* second = NULL;
-        f2(root,i-1,j-1,arr,first,second);
-        
+        prev = root;
+        f(root->right,prev,first,second);
+    }
+    
+    void recoverTree(TreeNode* root) {
+        TreeNode* prev = NULL, *first = NULL, *second = NULL;
+        f(root,prev,first,second);
         swap(first->val,second->val);
-        
-        //return one;
     }
 };
