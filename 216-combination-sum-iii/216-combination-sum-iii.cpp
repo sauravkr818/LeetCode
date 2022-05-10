079d46a967dcd543e186e82242b1a83c4a49fe55
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-05-10 10:41:25 +0530

    Time: 2 ms (51.79%), Space: 6.4 MB (58.80%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    
-    void permute(vector<int>&temp,int t, int k, int n){
+    void permute(vector<int>&temp,vector<vector<int>>&ans,int t, int k, int n){
         // base cases
         if(n<0 || temp.size()>k){
             return; 
@@ -14,15 +12,15 @@ public:
         
         for(int i=t;i<=9;i++){
             temp.push_back(i);
-            permute(temp,i+1,k,n-i);
+            permute(temp,ans,i+1,k,n-i);
             temp.pop_back();
         }
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
-        // vector<vector<int>>ans;
+        vector<vector<int>>ans;
         vector<int>temp;
-        permute(temp,1,k,n);
+        permute(temp,ans,1,k,n);
         return ans;
     }
 };
