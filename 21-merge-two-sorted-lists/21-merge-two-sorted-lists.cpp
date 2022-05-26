36dfb5130f4005be49fe8c289dfc11e297958744
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-05-26 16:02:59 +0530

    Time: 20 ms (8.52%), Space: 15.1 MB (8.65%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,30 +10,55 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* t = new ListNode();
-        ListNode* m = t;
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-        while(l1 && l2){
-            if(l1->val < l2->val){
-                m->next = l1;
-                l1 = l1->next;
-            }
-            else{
-                 m->next = l2;
-                 l2 = l2->next;
-            }
-            m = m->next;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* t1 = list1;
+        ListNode* t2 = list2;
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+        ListNode* ans;
+        ListNode* ans2;
+        if(t1->val>t2->val){
+            ans = new ListNode(t2->val);
+            t2 = t2->next;
+            ans2 = ans;
+        }
+        else{
+            ans = new ListNode(t1->val);
+            t1 = t1->next;
+            ans2 = ans;
         }
         
-        if(!l1){
-            m->next = l2;
+        int t = 1;
+        while(t1 != NULL && t2 != NULL){
+            if(t1->val>t2->val){
+                ans->next = new ListNode(t2->val);
+                //ans = ans->next;
+                t2 = t2->next;
+            }
+            else if(t1->val<t2->val){
+                ans->next = new ListNode(t1->val);
+                //ans = ans->next;
+                t1 = t1->next;
+            }
+            else if(t1->val == t2->val){
+                ans->next = new ListNode(t2->val);
+                
+                    ans = ans->next;
+                    ans->next = new ListNode(t2->val);
+                
+                //ans = ans->next;
+                t2 = t2->next;
+                t1 = t1->next;
+            }
+            t++;
+            ans = ans->next;
         }
-        if(!l2){
-            m->next = l1;
+        if(t1 == NULL){
+            ans->next = t2;
         }
-        
-        return t->next;
+        else{
+            ans->next = t1;
+        }
+        return ans2;
     }
 };
