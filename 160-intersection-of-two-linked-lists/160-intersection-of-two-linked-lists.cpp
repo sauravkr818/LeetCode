4ff8c0d5e9104fcd2083b165afadf5186b76acce
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-05-26 21:58:49 +0530

    Time: 68 ms (40.10%), Space: 14.4 MB (88.85%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,47 +9,37 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*>h1;
-        unordered_set<ListNode*>h2;
-        while(headA and headB){
-            if(h2.count(headA) == 1){
-                return headA;
-            }
-            else{
-                h1.insert(headA);
-            }
-            if(h1.count(headB) == 1){
-                return headB;
-            }
-            else{
-                h2.insert(headB);
-            }
+        int len1 = 0;
+        int len2 = 0;
+        ListNode* hA = headA;
+        ListNode* hB = headB;
+        while(headA){
+            len1++;
             headA = headA->next;
+        }
+        while(headB){
+            len2++;
             headB = headB->next;
         }
-        if(headA == NULL && headB == NULL)
-        return NULL;
-        else if(headA == NULL){
-            while(headB!=NULL){
-                if(h1.count(headB) == 1){
-                return headB;
-                }
-                else{
-                    h2.insert(headB);
-                }
-                    headB = headB->next;
+        cout<<len1<<" "<<len2;
+        int diff = 0;
+        if(len1>len2){
+            diff = len1-len2;
+            while(diff--){
+                hA = hA->next;
             }
         }
-        else if(headB == NULL){
-            while(headA!=NULL){
-                if(h2.count(headA) == 1){
-                return headA;
-                }
-                else{
-                    h1.insert(headA);
-                }
-                    headA = headA->next;
+        else{
+            diff = len2-len1;
+            while(diff--){
+                hB = hB->next;
             }
+        }
+        while(hA && hB){
+            if(hA == hB) return hA;
+            hA = hA->next;
+            hB = hB->next;
+            
         }
         return NULL;
     }
