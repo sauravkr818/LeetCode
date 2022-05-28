62aa2d636ee32879b5bb08341539926f78b06216
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-05-28 15:41:50 +0530

    Time: 16 ms (18.32%), Space: 11.8 MB (56.69%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -11,35 +11,32 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(k == 0) return head;
         if(head == NULL) return NULL;
-        ListNode* temp = head;
         int n = 0;
-        while(temp->next != NULL){
+        ListNode* temp = head;
+        while(temp){
             temp = temp->next;
-            n += 1;
-            //cout<<n<<endl;
+            n++;
+            
         }
-        ListNode* last = temp;
         temp = head;
-        n++;
-        cout<<n<<endl;
-        if(n == 1 || k % n == 0) return head;
-        if(k>=n) k = k%n;
-        int nok = n-k-1;
-        while(nok--){
+        int actual = k%n;
+        if(actual == 0) return head;
+        int it = n-actual-1;
+        while(it--){
             temp = temp->next;
         }
-        cout<<temp->val<<endl;
-        ListNode* temp2 = temp;
-        
-        temp = temp->next;
-        ListNode* ans = temp;
-        temp2->next = NULL;
-        
-        last->next = head;
-        
+        ListNode* save = temp;
+        save = save->next;
+        ListNode* ans = save;
+        ListNode* save2 = new ListNode(0);
+        temp->next = NULL;
+        while(save){
+            save2->next = save;
+            save2 = save2->next;
+            save = save->next;
+        }
+        save2->next = head;
         return ans;
-        
     }
 };
