f3c22703b5dbf6b9f8a8d3ce25082370e3818cee
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-05-31 02:08:31 +0530

    Time: 15 ms (80.79%), Space: 13.1 MB (95.93%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,79 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_set<int>s;
-        unordered_set<int>s1;
         int m = matrix.size();
         int n = matrix[0].size();
+        int col = true;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            if(matrix[i][0] == 0) col = false;
+            for(int j=1;j<n;j++){
                 if(matrix[i][j] == 0){
-                    s.insert(i);
-                    s1.insert(j);
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                // if(matrix[i][j] == 0){
-                    if(s.find(i) != s.end()){
-                        if(j>0) matrix[i][j-1] = 0;
-                        if(j<n-1) matrix[i][j+1] = 0;
-                        matrix[i][j] = 0;
-                    }
-                    if(s1.find(j) != s.end()){
-                        if(i>0) matrix[i-1][j] = 0;
-                        if(i<m-1) matrix[i+1][j] = 0;
-                        matrix[i][j] = 0;
-                    }
-                // }
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=1;j--){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
             }
+            if(!col) matrix[i][0] = 0;
         }
-        
-//         for(int i=0;i<m;i++){
-//             for(int j=n-1;j>=0;j--){
-//                 if(matrix[i][j] == 0){
-//                     if(s.find(i) != s.end()){
-//                         if(j>0) matrix[i][j-1] = 0;
-//                         if(j<n-1) matrix[i][j+1] = 0;
-//                     }
-//                     if(s1.find(j) != s.end()){
-//                         if(i>0) matrix[i-1][j] = 0;
-//                         if(i<m-1) matrix[i+1][j] = 0;
-//                     }
-//                 }
-//             }
-//         }
-        
-//         for(int i=m-1;i>=0;i--){
-//             for(int j=n-1;j>=0;j--){
-//                 if(matrix[i][j] == 0){
-//                     if(s.find(i) != s.end()){
-//                         if(j>0) matrix[i][j-1] = 0;
-//                         if(j<n-1) matrix[i][j+1] = 0;
-//                     }
-//                     if(s1.find(j) != s.end()){
-//                         if(i>0) matrix[i-1][j] = 0;
-//                         if(i<m-1) matrix[i+1][j] = 0;
-//                     }
-//                 }
-//             }
-//         }
-        
-//         for(int i=m-1;i>=0;i--){
-//             for(int j=0;j<n;j++){
-//                 if(matrix[i][j] == 0){
-//                     if(s.find(i) != s.end()){
-//                         if(j>0) matrix[i][j-1] = 0;
-//                         if(j<n-1) matrix[i][j+1] = 0;
-//                     }
-//                     if(s1.find(j) != s.end()){
-//                         if(i>0) matrix[i-1][j] = 0;
-//                         if(i<m-1) matrix[i+1][j] = 0;
-//                     }
-//                 }
-//             }
-//         }
-        
     }
 };
