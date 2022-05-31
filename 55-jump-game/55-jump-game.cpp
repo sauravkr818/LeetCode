24b4cbfe4b751d48cef2975ca7f8f0d7ecd7220a
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-05-31 16:06:34 +0530

    Time: 110 ms (36.58%), Space: 48.5 MB (44.51%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -3,16 +3,12 @@ public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
         // edge cases
-        if(nums[0] == 0 && n>1) return false;
-        else if(nums[0] == 0 && n == 1) return true;
-        
-        int dist = 0;
-        for(int i=0;i<=dist;i++){
-            dist = max(dist,i+nums[i]);
-            if(dist>=n-1) return true;
+        int max_ind = 0;
+        for(int i=0;i<n;i++){
+            if(i>max_ind) return false;
+            if(i+nums[i]>max_ind) max_ind = i+ nums[i];
         }
-        
-        return false;
+        return true;
         
     }
 };
