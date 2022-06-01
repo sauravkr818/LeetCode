73112c239d0a97e468ab584be774ce5a4dd73dcb
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-06-01 19:05:00 +0530

    Time: 47 ms (63.63%), Space: 20.2 MB (15.68%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& matrix) {
+        sort(matrix.begin(),matrix.end());
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>>ans = {matrix[0]};
+        for(int i=1;i<m;i++){
+            int val = ans.size();
+            vector<int>temp = matrix[i];
+            if(ans[val-1][1]>=matrix[i][0] && ans[val-1][1]>=matrix[i][1]){
+                // ans.pop_back();
+                // vector<int>t = {};
+                // ans.push_back()
+            }
+            else if(ans[val-1][1]>=matrix[i][0] && ans[val-1][1]<matrix[i][1]){
+                vector<int>t = {ans[val-1][0],matrix[i][1]};
+                ans.pop_back();
+                ans.push_back(t);
+            }
+            else{
+                ans.push_back(temp);
+            }
+        }
+        return ans;
+    }
+};
