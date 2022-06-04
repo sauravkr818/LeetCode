5dcd8bdbbd3de2631f5d4ecbf3a4588a5ad0bb9b
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-06-04 14:02:43 +0530

    Time: 101 ms (7.92%), Space: 47.4 MB (7.44%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,83 @@
+class Solution {
+public:
+    
+    bool isSafe(int row, int col,int n, vector<string>board){
+        // inorder to check whether placing a queen at index [row][col] we should know 
+        // these facts:
+        // 1. Every col has exactly one queen and every row has exactly one queen.
+        // 2. You are traversing from col = 0 hence it is not necessary to check for right-diagonals and right side
+        // 3. You are only required to check the left-diagonals and left side
+        // 4. Also since you are travelling from row=0 to row = n-1 and you know only one queen is to be placed in a column hence you need not check up and down direction as well
+        // 5. therefore directions to travel
+        //          .
+        //           .
+        //            .
+        //       <-----[row,col]
+        //            .
+        //           .
+        //          .
+        
+        // hence you must check in only these directions
+        
+        // ------------ checking left-up diagonal --------------
+        
+        int duprow = row;
+        int dupcol = col;
+        
+        while(row>=0 && col>=0){
+            
+            if( board[row][col] == 'Q') return false;
+            row--;col--;
+        }
+        
+        // --------------- checking left -------------------
+        
+        row = duprow;
+        col = dupcol;
+        while(col>=0){
+            
+            if(board[row][col] == 'Q') return false;
+            col--;
+        }
+        
+        // ------------ checking left-down diagonal --------------
+        
+        col = dupcol;
+        while(row<n && col>=0){
+            
+            if(board[row][col] == 'Q') return false;
+            row++;col--;
+        }
+        
+        return true;
+        
+        
+    }
+    
+    void solve(int col, int n, vector<string>&board,vector<vector<string>>&ans)
+    {
+        // base case
+        if(col == n){
+            ans.push_back(board);
+        }
+        
+        for(int row = 0; row<n; row++){
+            if(isSafe(row,col,n,board)){
+                board[row][col] = 'Q';
+                solve(col+1,n,board,ans);
+                board[row][col] = '.';
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string>board;
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board.push_back(s);
+        }
+        solve(0,n,board,ans);
+        return ans;
+    }
+};
