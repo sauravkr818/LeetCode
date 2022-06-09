f3dbee257508686ff36e12b53a3146c8dd369d6a
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-06-09 18:39:52 +0530

    Time: 54 ms (49.12%), Space: 6.4 MB (96.01%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -36,24 +36,47 @@ public:
         
         
         // ------------- tabulation -----------------------
-        vector<vector<double>>dp(n+1, vector<double>(m+1,0));
         
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
-        }
+//         vector<vector<double>>dp(n+1, vector<double>(m+1,0));
         
-        // just to not overwrite the values written by above for loop , start j from 1
-        for(int j=1;j<=m;j++){
-            dp[0][j] = 0;
-        }
+//         for(int i=0;i<=n;i++){
+//             dp[i][0] = 1;
+//         }
         
+//         // just to not overwrite the values written by above for loop , start j from 1
+//         for(int j=1;j<=m;j++){
+//             dp[0][j] = 0;
+//         }
+        
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=m;j++){
+//                 if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+//                 else dp[i][j] = dp[i-1][j];
+//             }
+//         }
+        
+//         return (int)dp[n][m];
+        
+        
+        
+        // ----------------- tabulation + space optimisation to 2 1D array
+        
+        
+        vector<double>prev(m+1,0),cur(m+1,0);
+        
+        prev[0] = 1;
+        cur[0] = 1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                else dp[i][j] = dp[i-1][j];
+                if(s[i-1] == t[j-1]) cur[j] = prev[j-1] + prev[j];
+                else cur[j] = prev[j];
             }
+            prev = cur;
         }
         
-        return (int)dp[n][m];
+        return (int)prev[m];
+        
+    
+        
     }
 };
