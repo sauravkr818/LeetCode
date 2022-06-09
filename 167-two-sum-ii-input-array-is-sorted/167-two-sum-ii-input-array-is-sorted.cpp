d41af8caa25f6706f07464c29ecf5d38da7e1156
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-06-09 14:49:47 +0530

    Time: 153 ms (5.14%), Space: 15.7 MB (44.00%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        int ans1 = 0 ,ans2 = 0;
+        for(int i=0;i<n;i++){
+            int a = nums[i];
+            int b = target - a;
+            cout<<a<<" "<<b<<endl;
+            ans1 = i+1;
+            if(lower_bound(nums.begin()+i+1, nums.end(), b) != nums.end()){
+                ans2 = (lower_bound(nums.begin()+i+1, nums.end(), b)) - nums.begin();
+                cout<<ans2<<endl;
+                cout<<"nums :"<<nums[ans2]<<"target :"<<b<<endl;
+                if(nums[ans2] != b){
+                    continue;
+                }
+                break;
+            }
+            
+        }
+        vector<int>arr = {ans1, ans2+1};
+        return arr;
+    }
+};
