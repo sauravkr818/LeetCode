6be6b151e530e4041fe1320779403bf27b596366
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-06-10 21:53:39 +0530

    Time: 19 ms (54.51%), Space: 10.9 MB (15.69%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -18,10 +18,34 @@ public:
     }
     
     
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<double>>dp(n, vector<double>(m,-1));
-        return (int) f(n-1,m-1,word1,word2,dp);
+    int minDistance(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        // vector<vector<double>>dp(n, vector<double>(m,-1));
+        // return (int) f(n-1,m-1,word1,word2,dp);
+        
+        
+        vector<vector<double>>dp(n+1, vector<double>(m+1,0));
+        
+        for(int j=0;j<=m;j++){
+            dp[0][j] = j;
+        }
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0] = i;
+        }
+        
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1] == s2[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
+                else{
+                    dp[i][j] = min( 1 + dp[i][j-1], 
+                                   min( 1 + dp[i-1][j], 1 + dp[i-1][j-1] ));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
