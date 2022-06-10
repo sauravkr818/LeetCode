fba0b93880b37f81c501fc261f87e425740fe073
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-06-10 20:13:55 +0530

    Time: 42 ms (68.51%), Space: 6.5 MB (88.90%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -62,21 +62,33 @@ public:
         // ----------------- tabulation + space optimisation to 2 1D array
         
         
-        vector<double>prev(m+1,0),cur(m+1,0);
+//         vector<double>prev(m+1,0),cur(m+1,0);
+        
+//         prev[0] = 1;
+//         cur[0] = 1;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=m;j++){
+//                 if(s[i-1] == t[j-1]) cur[j] = prev[j-1] + prev[j];
+//                 else cur[j] = prev[j];
+//             }
+//             prev = cur;
+//         }
+        
+//         return (int)prev[m];
+        
+    // ----------------- tabulation + space optimisation to 1 1D array
+        
+        vector<double>prev(m+1,0);
         
         prev[0] = 1;
-        cur[0] = 1;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1] == t[j-1]) cur[j] = prev[j-1] + prev[j];
-                else cur[j] = prev[j];
+            for(int j=m;j>=1;j--){
+                if(s[i-1] == t[j-1]) prev[j] = prev[j-1] + prev[j];
+                
             }
-            prev = cur;
+           
         }
         
         return (int)prev[m];
-        
-    
-        
     }
 };
