9de043257cc8736e0334b142e33c5447a8c61a33
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-07-02 21:52:55 +0530

    Time: 836 ms (12.91%), Space: 10.7 MB (18.70%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -22,20 +22,37 @@ public:
         // return f(0,-1,nums,n,dp);
         
         // tabulation
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+//         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+//         for(int ind = n-1; ind>=0;ind--){
+//             for(int prev_ind = ind-1; prev_ind>=-1;prev_ind--){
+//                 int not_take = 0; // not_take case
+//                 not_take = 0 + dp[ind+1][prev_ind+1];
+
+//                 int take = 0; // take case
+//                 if(prev_ind == -1 || nums[ind]>nums[prev_ind]){
+//                 take = 1 + dp[ind+1][ind+1];
+//                 }
+//                 dp[ind][prev_ind+1] = max(take, not_take);
+//             }
+//         }
+        
+//         return dp[0][-1+1];
+        
+        vector<int>next(n+1,0),cur(n+1,0);
         for(int ind = n-1; ind>=0;ind--){
             for(int prev_ind = ind-1; prev_ind>=-1;prev_ind--){
                 int not_take = 0; // not_take case
-                not_take = 0 + dp[ind+1][prev_ind+1];
+                not_take = 0 + next[prev_ind+1];
 
                 int take = 0; // take case
                 if(prev_ind == -1 || nums[ind]>nums[prev_ind]){
-                take = 1 + dp[ind+1][ind+1];
+                take = 1 + next[ind+1];
                 }
-                dp[ind][prev_ind+1] = max(take, not_take);
+                cur[prev_ind+1] = max(take, not_take);
             }
+            next = cur;
         }
         
-        return dp[0][-1+1];
+        return cur[-1+1];
     }
 };
