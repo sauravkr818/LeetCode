a9483114fdf8bee80472eb9135f3429275241267
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-07-13 14:30:05 +0530

    Time: 19 ms (90.99%), Space: 19.6 MB (12.88%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    // Boyer-Moore Voting Algorithm tc = O(n) & sc = O(1)
+    
+    // Therefore, given that it is impossible (in both cases) to discard more majority elements than minority elements, we are safe in discarding the prefix and attempting to recursively solve the majority element problem for the suffix. Eventually, a suffix will be found for which count does not hit 0, and the majority element of that suffix will necessarily be the same as the majority element of the overall array.
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        int candidate = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+};
