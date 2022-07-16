46454e29102a9e9a0dbaf79e8060031095ce8650
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-07-16 20:56:46 +0530

    Time: 1112 ms (14.57%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,8 +1,21 @@
 /* Write your T-SQL query statement below */
+
+-- using left join
+
+-- select name as Customers from Customers
+-- left join Orders
+-- on Customers.id = Orders.customerId
+-- where Orders.customerId is null;
+
+-- using `not in` clause and nested query
+
 select name as Customers from Customers
-left join Orders
-on Customers.id = Orders.customerId
-where Orders.customerId is null;
+where Customers.id not in (
+    select customerId from Orders
+);
+
+
+
 
 
 
