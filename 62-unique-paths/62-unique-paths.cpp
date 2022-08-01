992ebe091842f1ff5da0b7ff76f1c36bae3a8533
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-08-01 12:57:10 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (63.21%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -14,36 +14,35 @@ public:
         // return f(m-1,n-1,dp);
         
         // tabulation
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i == 0 && j == 0) dp[i][j] = 1;
-                else{
-                    int up = 0;
-                    int left = 0;
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
-                    dp[i][j] = up+left;
-                }
-            }
-        }
-        return dp[m-1][n-1];
-        
-        // tabulation + space optimisation
-        // vector<int>prev(m,0);
-        // for(int i=0;i<n;i++){
-        //     vector<int>curr(m,0);
-        //     for(int j=0;j<m;j++){
-        //         if(i==0 && j ==0) curr[j] = 1;
+        // vector<vector<int>>dp(m,vector<int>(n,0));
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i == 0 && j == 0) dp[i][j] = 1;
         //         else{
-        //             int up,left = 0;
-        //             if(i>0) up = prev[j];
-        //             if(j>0) left = curr[j-1];
-        //             curr[j] = up+left;
+        //             int up = 0;
+        //             int left = 0;
+        //             if(i>0) up = dp[i-1][j];
+        //             if(j>0) left = dp[i][j-1];
+        //             dp[i][j] = up+left;
         //         }
         //     }
-        //     prev = curr;
         // }
-        // return prev[m-1];
+        // return dp[m-1][n-1];
+        
+        // tabulation + space optimisation
+        vector<int>prev(n,1), cur(n,1);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(i==0 && j ==0) cur[j] = 1;
+                else{
+                    int up,left = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = cur[j-1];
+                    cur[j] = up+left;
+                }
+            }
+            prev = cur;
+        }
+        return prev[n-1];
     }
 };
