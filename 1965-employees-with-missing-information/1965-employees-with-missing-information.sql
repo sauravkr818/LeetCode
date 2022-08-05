b86d3678e0fd934be3ed989e4ae87361bfb1e7d9
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-08-05 23:40:11 +0530

    Time: 959 ms (17.01%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,2 +1,7 @@
 /* Write your T-SQL query statement below */
-select employee_id from Employees union select employee_id from Salaries except select Employees.employee_id from Employees inner join Salaries on Employees.employee_id = Salaries.employee_id;
+-- select employee_id from Employees union select employee_id from Salaries except select Employees.employee_id from Employees inner join Salaries on Employees.employee_id = Salaries.employee_id;
+
+select employee_id from Employees where employee_id not in(select employee_id from Salaries)
+union
+select employee_id from Salaries where employee_id not in (select employee_id from Employees)
+order by employee_id;
