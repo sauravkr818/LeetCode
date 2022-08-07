99fe6b8e2e5666f517c5935735dbbd8f755b7f17
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-08-07 14:28:09 +0530

    Time: 43 ms (62.62%), Space: 15.9 MB (52.34%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,69 +1,45 @@
 class Solution {
 public:
-//     int dp[100][20001];
-//     int f(char last, int n){
-//         // base cases
-//         if(n == 0) {
-//             return 1;
-//         }
-        
-//         // take
-//         int take = 0;
-//         if(last == '$'){
-            
-//             take = f('a',n-1) + f('e',n-1) + f('i',n-1) + f('o',n-1) + f('u',n-1);
-//         }
-//         else if(last == 'a'){
-//             take = f('e',n-1);
-//         }
-//         else if(last == 'e'){
-//             take = f('a',n-1) + f('i',n-1);
-//         }
-//         else if(last == 'i'){
-//             take = f('a',n-1) + f('e',n-1) + f('o',n-1) + f('u',n-1);
-//         }
-//         else if(last == 'o'){
-//             take = f('i',n-1) + f('u',n-1);
-//         }
-//         else if(last == 'u'){
-//             take = f('a',n-1);
-//         }
-        
-        
-//         return dp[abs(last-'a')][n] = take;
-            
-//     }
-//     int countVowelPermutation(int n) {
-//         memset(dp,-1,sizeof(dp));
-//         char last = '$';
-//         int t = f(last,n);
-        
-//         return t;
-//     }
-    
-    static const int a = 0, e = 1, i = 2, o = 3, u = 4, s = 5, MOD = 1e9 + 7;
-    const vector<vector<int>> mapping = {{e}, // a ->
-                                         {a, i}, // e -> 
-                                         {a, e, o, u}, // i ->
-                                         {i, u}, // o ->
-                                         {a}, // u ->
-                                         {a, e, i, u, o}}; // s ->
-    int dp[20001][6] = {};
-    
-    int f(int n, int last){
-        if (n == 0) return 1;
-        if (dp[n][last]) return dp[n][last];
-        int ans = 0;
-        for (int nxt : mapping[last]) {
-            ans = (ans + f(n-1, nxt)) % MOD;
+    int dp[100][20001];
+    static const int MOD = 1e9+7;
+    long long f(char last, int n){
+        // base cases
+        if(n == 0) {
+            return 1;
         }
-        return dp[n][last] = ans;
+        
+        if(dp[abs(last-'a')][n] != -1) return dp[abs(last-'a')][n];
+        // take
+        long long take = 0;
+        if(last == '$'){
+            
+            take = f('a',n-1) + f('e',n-1) + f('i',n-1) + f('o',n-1) + f('u',n-1);
+        }
+        else if(last == 'a'){
+            take = f('e',n-1);
+        }
+        else if(last == 'e'){
+            take = f('a',n-1) + f('i',n-1);
+        }
+        else if(last == 'i'){
+            take = f('a',n-1) + f('e',n-1) + f('o',n-1) + f('u',n-1);
+        }
+        else if(last == 'o'){
+            take = f('i',n-1) + f('u',n-1);
+        }
+        else if(last == 'u'){
+            take = f('a',n-1);
+        }
+        
+        
+        return dp[abs(last-'a')][n] = take % (MOD);
+            
     }
-    int countVowelPermutation(int n) {
+    long long countVowelPermutation(int n) {
+        memset(dp,-1,sizeof(dp));
+        char last = '$';
+        long long t = f(last,n);
         
-        int last = 5;
-        int t = f(n,last);
-        
-        return t;
+        return t%MOD;
     }
 };
