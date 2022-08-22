100474206fb3959ca5462e45725383d9b8495e21
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-08-22 11:18:35 +0530

    Time: 55 ms (63.31%), Space: 20.7 MB (11.91%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& matrix) {
-        sort(matrix.begin(),matrix.end());
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>>ans = {matrix[0]};
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        int m = nums.size();
+        int n = nums[0].size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans = {nums[0]};
         for(int i=1;i<m;i++){
             int val = ans.size();
-            vector<int>temp = matrix[i];
-            if(ans[val-1][1]>=matrix[i][0] && ans[val-1][1]>=matrix[i][1]){
+            if(nums[i][0]>ans[val-1][1]) {
+                ans.push_back({nums[i][0],nums[i][1]});
                 continue;
             }
-            else if(ans[val-1][1]>=matrix[i][0] && ans[val-1][1]<matrix[i][1]){
-                vector<int>t = {ans[val-1][0],matrix[i][1]};
+            else if(nums[i][0]<=ans[val-1][1]){
+                vector<int>arr1;
+                arr1.push_back(ans[val-1][0]);
+                arr1.push_back(max(ans[val-1][1],nums[i][1]));
                 ans.pop_back();
-                ans.push_back(t);
-            }
-            else{
-                ans.push_back(temp);
+                ans.push_back(arr1);
             }
         }
         return ans;
