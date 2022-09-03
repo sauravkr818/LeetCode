0ac981b5671861a444075d3be44df6f1ac3aa44c
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-09-04 00:24:10 +0530

    Time: 642 ms (20.00%), Space: 128.8 MB (40.00%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -4,31 +4,18 @@ public:
         int i = 0, j = 0;
         int n = ct.size();
         priority_queue<pair<long long,int>>pq;
-        long long sum = 0;
         long long gg = 0;
-        unordered_set<int>st;
         long long count = 0;
         while(j<n){
-            
-            pq.push({(long long)ct[j],j});
-            
-            gg += (long long)rc[j];
-            
-                
-            
-             
-        while(pq.empty() == false && (pq.top()).first + (long long)((j-i+1) * gg)>bgt && i<n){
-                    gg -= (long long)rc[i];
-                    
-                 if ((pq.top()).second <= i){
-                        pq.pop();
-                 }
-                     
-                    
-                    i++;
-                    
+            pq.push({ct[j],j});
+            gg += rc[j];
+            while(pq.empty() == false && (pq.top()).first + ((j-i+1) * gg)>bgt && i<n){
+                gg -= rc[i];
+                if ((pq.top()).second <= i){
+                    pq.pop();
                 }
-               
+                i++;
+            }
             count = max(count,(long long)j-i+1);
             j++;
         }
