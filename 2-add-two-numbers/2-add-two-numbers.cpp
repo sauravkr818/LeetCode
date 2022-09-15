471940d966b9d8ba530e5cee1a8a544a037f0920
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-09-16 00:42:46 +0530

    Time: 63 ms (41.76%), Space: 71.5 MB (12.65%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,75 +11,41 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* temp = new ListNode();
+        ListNode* ans = temp;
         int carry = 0;
-        ListNode* ans = new ListNode();ListNode* t = ans;
         while(l1 && l2){
-            int sum = l1->val + l2->val + carry;
-            int rem = sum%10;
-            if(sum<10){
-                carry = 0;
-            }
-            ListNode* temp;
-            if(sum<10)
-            temp = new ListNode(sum);
-            else{
-                temp = new ListNode(rem);
-                carry = 1;
-            }
-            ans->next = temp;
-            ans = ans->next;
+            int sum = l1->val + l2->val;
+            int rem = (sum+carry) % 10;
+            carry = (sum+carry) / 10;
+            ListNode *t = new ListNode(rem);
+            temp->next = t;
+            
             l1 = l1->next;
             l2 = l2->next;
+            temp = temp->next;
         }
         
-        if(l1 == NULL && l2 == NULL){
-            if(carry == 1){
-            ListNode* temp = new ListNode(1);
-            ans->next = temp;
+        while(l1){
+            int rem = (l1->val+carry) % 10;
+            carry = (l1->val+carry) / 10;
+            ListNode *t = new ListNode(rem);
+            temp->next = t;
+            l1 = l1->next;
+            temp = temp->next;
+            
         }
-            return t->next;
+        while(l2){
+            int rem = (l2->val+carry) % 10;
+            carry = (l2->val+carry) / 10;
+            ListNode *t = new ListNode(rem);
+            temp->next = t;
+            l2 = l2->next;
+            temp = temp->next;
         }
-        else if(l1 == NULL){
-            while(l2 != NULL){
-                int sum = l2->val + carry;
-                if(sum>=10){
-                    carry = 1;
-                    int rem = sum%10;
-                    ListNode* temp = new ListNode(rem);
-                    ans->next = temp;
-                }
-                else{
-                    carry = 0;
-                    ListNode* temp = new ListNode(sum);
-                    ans->next = temp;
-                }
-                ans = ans->next;
-                l2 = l2->next;
-            }
+        if(carry != 0){
+            temp->next = new ListNode(carry);
         }
-        else{
-            while(l1 != NULL){
-                int sum = l1->val + carry;
-                if(sum>=10){
-                    carry = 1;
-                    int rem = sum%10;
-                    ListNode* temp = new ListNode(rem);
-                    ans->next = temp;
-                }
-                else{
-                    carry = 0;
-                    ListNode* temp = new ListNode(sum);
-                    ans->next = temp;
-                }
-                ans = ans->next;
-                l1 = l1->next;
-            }
-        }
-        
-        if(carry == 1){
-            ListNode* temp = new ListNode(1);
-            ans->next = temp;
-        }
-        return t->next;
+        return ans->next;
     }
 };
