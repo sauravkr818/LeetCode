a3faac00e1aa4a65d2ad8db878a33e8eac886511
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-09-16 10:55:05 +0530

    Time: 772 ms (37.79%), Space: 120 MB (66.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int i,int n,int k,vector<int> &nums,vector<int> &mums){
-        if(k==mums.size()){
-            return 0;
-        }
-        if(dp[i][k]!=INT_MIN)return dp[i][k];
-        int x=solve(i+1,n,k+1,nums,mums)+(nums[i]*mums[k]);
-        int y=solve(i,n,k+1,nums,mums)+(nums[(n-1)-(k-i)]*mums[k]);
-        return dp[i][k]=max(x,y);
+    int f(int i,int k,vector<int>&nums,vector<int>&mul,vector<vector<int>>&dp){
+        // base cases
+        if(i>=nums.size() || i>=mul.size() || k>=mul.size()) return 0;
+        
+        if(dp[i][k] != INT_MIN) return dp[i][k];
+        
+        int j = nums.size() - 1 - (k - i);
+        int start = (nums[i] * mul[k]) + f(i+1,k+1,nums,mul,dp);
+        int end = (nums[j] * mul[k]) + f(i,k+1,nums,mul,dp);
+        return dp[i][k] = max(start,end);
     }
-    int maximumScore(vector<int>& nums, vector<int>& mums) {
-        int n=nums.size(), m=mums.size();
-        dp.resize(m+1,vector<int>(m+1,INT_MIN));
-        return solve(0,n,0,nums,mums);
+    int maximumScore(vector<int>& nums, vector<int>& mul) {
+        int n = nums.size();
+        int m = mul.size();
+        int i = 0;
+        int k = 0;
+        vector<vector<int>>dp(m,vector<int>(m,INT_MIN));
+        return f(i,k,nums,mul,dp);
     }
 };
