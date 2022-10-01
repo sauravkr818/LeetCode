cd88dbab324ba71020754e735ada2f05ce88f2f6
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-10-01 18:31:47 +0530

    Time: 3 ms (63.51%), Space: 6.3 MB (28.47%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // memoization
     int f(int i, int n, string &s,vector<int>&dp){
         // base case
         if(i == n) return 1;
@@ -18,7 +19,21 @@ public:
     }
     int numDecodings(string s) {
         int n = s.size();
-        vector<int>dp(n,-1);
-        return f(0,n,s,dp);
+        // vector<int>dp(n,-1);
+        // return f(0,n,s,dp);
+        
+        vector<int>dp(n+1,0);
+        dp[n] = 1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i] == '0') continue;
+            int single = dp[i+1];
+            // pick 2 letters or couple
+            int coupl = 0;
+            if(i+1<n && ((s[i] == '1' && s[i+1] <= '9') || (s[i] == '2' && s[i+1] <='6'))){
+                coupl = dp[i+2];
+            }
+            dp[i] = single + coupl;
+        }
+        return dp[0];
     }
 };
