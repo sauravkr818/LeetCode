d525a7eb3f9b326c72bfe560f4d1026ff10b07b8
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-10-08 11:12:16 +0530

    Time: 97 ms (88.61%), Space: 21 MB (48.14%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    // Two-pointers approach
-    // Time - O(n*n)
-    // space - O(m) - ans array that is returned at last;
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>ans;
-        if(n<3) return ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i++){
-            int a = nums[i];
-            if(a>0) break;
-            // a+b+c = 0;
-            // therefore b+c = -a which is a two-sum problem
-            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
-                int lo = i+1;
-                int hi = n-1;
-                while(lo<hi){
-                    if(nums[lo]+nums[hi] == -a){
-                        vector<int>arr = {nums[lo],nums[hi],a};
-                        ans.push_back(arr);
-                        while (lo < hi && nums[lo] == nums[lo+1]) lo++;
-                        while (lo < hi && nums[hi] == nums[hi-1]) hi--;
-                        lo++;
-                        hi--;
-
-                    }
-                    else if(nums[lo]+nums[hi] < -a){
-                        lo++;
-                    }
-                    else if(nums[lo]+nums[hi] > -a){
-                        hi--;
-                    }
+        //for(auto t: nums) cout<<t<<" ";
+        vector<vector<int>>ans;
+        int n = nums.size();
+        if(n<3) return ans;
+        int prev = INT_MIN;
+        for(int i=0;i<n;i++){
+            if(prev == nums[i]) continue;
+            prev = nums[i];
+            int target = -1 * nums[i];
+            int lo = i+1;
+            int hi = n-1;
+            while(lo<hi){
+                if(nums[lo] + nums[hi] == target){
+                    vector<int>temp = {nums[lo],nums[hi],-1* target};
+                    ans.push_back(temp);
+                    while (lo < hi && nums[lo] == nums[lo+1]) lo++;
+                    while (lo < hi && nums[hi] == nums[hi-1]) hi--;
+                    lo++;
+                    hi--;
+                }
+                else if(nums[lo] + nums[hi] > target){
+                    hi--;
+                }
+                else{
+                    lo++;
                 }
             }
         }
