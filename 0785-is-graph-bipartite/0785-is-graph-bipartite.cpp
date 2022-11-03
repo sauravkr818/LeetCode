4097e1dd63051fed2e07a581f246554aa08875bf
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-11-03 22:38:07 +0530

    Time: 55 ms (54.28%), Space: 13.5 MB (84.32%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
+    bool dfs(int src,int col, vector<int>&vis, vector<vector<int>>& adj, int V){
+        vis[src] = col;
+        for(auto node: adj[src]){
+            if(vis[node] == -1){
+                if(dfs(node,!col,vis,adj,V) == false) return false;
+            }
+            else if(vis[node] == col) return false;
+        }
+        return true;
+    }
+    
     bool isBipartite(vector<vector<int>>& adj) {
         int V = adj.size();
         vector<int>vis(V,-1);
-	    queue<int>q;
+        for(int i=0;i<V;i++){
+            if(vis[i] == -1){
+                vis[i] = 0;
+                if(dfs(i,0,vis,adj,V) == false){
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+        // -- bfs --
+// 	    queue<int>q;
 	    
-	    for(int i=0;i<V;i++){
-	        if(vis[i] == -1){
-	            q.push(i);
-        	    vis[i] = 0;
-	            while(!q.empty()){
-        	        int node = q.front();
-        	        q.pop();
-        	        for(auto it:adj[node]){
-        	            if(vis[it] == -1){
-        	                vis[it] = 1-vis[node];
-        	                q.push(it);
-        	            }
-        	            else{
-        	                if(vis[node] == vis[it]) return false;
-        	            }
-        	        }
-        	    }
-	        }
-	    }
+// 	    for(int i=0;i<V;i++){
+// 	        if(vis[i] == -1){
+// 	            q.push(i);
+//         	    vis[i] = 0;
+// 	            while(!q.empty()){
+//         	        int node = q.front();
+//         	        q.pop();
+//         	        for(auto it:adj[node]){
+//         	            if(vis[it] == -1){
+//         	                vis[it] = 1-vis[node];
+//         	                q.push(it);
+//         	            }
+//         	            else{
+//         	                if(vis[node] == vis[it]) return false;
+//         	            }
+//         	        }
+//         	    }
+// 	        }
+// 	    }
 	    
-	    return true;
+// 	    return true;
     }
 };
