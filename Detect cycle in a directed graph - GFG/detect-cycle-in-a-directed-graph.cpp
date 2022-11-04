c5d0b7ecb33af7b4024621d701f4aa920962ce48
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-11-04 23:29:11 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -4,31 +4,64 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
+  private:
+//   bool dfs(int node, vector<int> adj[], int vis[], int pathVis[]){
+//       vis[node] = 1;
+//       pathVis[node] = 1;
+//       for(auto it: adj[node]){
+//           if(!vis[it]){
+//               if(dfs(it,adj,vis,pathVis)) return true;
+//           }
+//           else if(pathVis[it]) return true;
+//       }
+//       // remove from the path if the node is done
+//       pathVis[node] = 0;
+//       return false;
+//   }
   public:
     // Function to detect cycle in a directed graph.
-    
-    bool dfs(int u, vector<int>adj[], vector<bool>&visited, vector<bool>&recSt){
-        visited[u] = true;
-        recSt[u] = true;
-        for(auto v: adj[u]){
-            if(visited[v] == false){
-                if(dfs(v,adj,visited,recSt)) return true;
-            }
-            else if(recSt[v] == true) return true;
-        }
-        recSt[u] = false;
-        return false;
-    }
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        vector<bool>visited(V+1,false);
-        vector<bool>recSt(V+1,false);
-        for(int i=0;i<V;i++){
-            if(visited[i] == false){
-                if(dfs(i,adj,visited,recSt)) return true;
-            }
-        }
-        return false;
+        // 1st method - using dfs
+        
+        // int vis[V] = {0};
+        // int pathVis[V] = {0};
+        // for(int i=0;i<V;i++){
+        //     if(dfs(i,adj,vis,pathVis)) return true;
+        // }
+        // return false;
+        
+        // 2nd method - using bfs - kahn's algo - topo sort
+        int indegree[V] = {0};
+	   for(int i=0;i<V;i++){
+	       for(auto it: adj[i]){
+	           indegree[it]++;
+	       }
+	   }
+	   queue<int>q;
+	   for(int i=0;i<V;i++){
+	       if(indegree[i] == 0) {
+	           q.push(i);
+	       }
+	   }
+	   
+	   int temp = 0; // number of topo sort elements 
+	   // if temp != V means there was a cycle and if temp == V means it was a DAG.
+	   while(!q.empty()){
+	       int node = q.front();
+	       temp++;
+	       q.pop();
+	       for(auto it: adj[node]){
+	           indegree[it]--;
+	           if(indegree[it] == 0){
+	               q.push(it);
+	           }
+	       }
+	   }
+	   //return ans;
+	   if(temp != V) return true;
+	   return false;
+        
     }
 };
 
