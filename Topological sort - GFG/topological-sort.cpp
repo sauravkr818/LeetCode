34b3d40d1f99e41e1a99d8da60e41064a3897128
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-11-04 23:21:07 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -5,32 +5,62 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
+    
     private:
-    void dfs(int node, vector<int>adj[], int vis[], stack<int>&st){
-        vis[node] = 1;
-        for(auto it: adj[node]){
-            if(!vis[it]) dfs(it,adj,vis,st);
-        }
-        st.push(node);
-    }
+    // void dfs(int node, vector<int>adj[], int vis[], stack<int>&st){
+    //     vis[node] = 1;
+    //     for(auto it: adj[node]){
+    //         if(!vis[it]) dfs(it,adj,vis,st);
+    //     }
+    //     st.push(node);
+    // }
 	public:
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
-	    int vis[V] = {0};
-	    stack<int>st;
-	    vector<int>ans;
-	    for(int i=0;i<V;i++){
-	        
-	        if(!vis[i]) dfs(i,adj,vis,st);
-	    }
+	    // dfs and using a stack
 	    
-	    while(!st.empty()){
-	        ans.push_back(st.top());
-	        st.pop();
-	    }
-	    return ans;
+	   // int vis[V] = {0};
+	   // stack<int>st;
+	   // vector<int>ans;
+	   // for(int i=0;i<V;i++){
+	        
+	   //     if(!vis[i]) dfs(i,adj,vis,st);
+	   // }
+	    
+	   // while(!st.empty()){
+	   //     ans.push_back(st.top());
+	   //     st.pop();
+	   // }
+	   // return ans;
+	   
+	   int indegree[V] = {0};
+	   for(int i=0;i<V;i++){
+	       for(auto it: adj[i]){
+	           indegree[it]++;
+	       }
+	   }
+	   queue<int>q;
+	   for(int i=0;i<V;i++){
+	       if(indegree[i] == 0) {
+	           q.push(i);
+	       }
+	   }
+	   
+	   vector<int>ans;
+	   while(!q.empty()){
+	       int node = q.front();
+	       ans.push_back(node);
+	       q.pop();
+	       for(auto it: adj[node]){
+	           indegree[it]--;
+	           if(indegree[it] == 0){
+	               q.push(it);
+	           }
+	       }
+	   }
+	   return ans;
 	}
 };
 
