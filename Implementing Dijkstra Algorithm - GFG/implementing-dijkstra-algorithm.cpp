30bc960afd880d8b976cf967dbcf9b82b504723f
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-11-09 01:46:41 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -8,28 +8,68 @@ class Solution
 	public:
 	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
+    
+    // using priority queue
+    
+    // vector <int> dijkstra(int V, vector<vector<int>> adj[], int s)
+    // {
+    //     // Code here
+    //     // min-heap
+    //     priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>pq; // pair<dist,node>
+    //     vector<int>dist(V);
+    //     for(int i=0;i<V;i++) dist[i] = 1e9;
+        
+
+    //     dist[s] = 0;
+    //     pq.push({0,s});
+    //     while(!pq.empty()){
+    //         int node_dist = pq.top().first;
+    //         int node = pq.top().second;
+    //         pq.pop();
+    //         for(auto it: adj[node]){
+    //             // it[0] = adjacent node
+    //             // it[1] = edgeWeight req to reach node to adj node it
+    //             if(node_dist + it[1]< dist[it[0]]){
+    //                 dist[it[0]] = node_dist + it[1];
+    //                 pq.push({dist[it[0]], it[0]});
+    //             }
+    //         }
+    //     }
+    //     return dist;
+    // }
+    
+    // using set
+    
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int s)
     {
         // Code here
         // min-heap
-        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>pq; // pair<dist,node>
+        set<pair<int,int>>st;
         vector<int>dist(V);
         for(int i=0;i<V;i++) dist[i] = 1e9;
         
-
+        
         dist[s] = 0;
-        pq.push({0,s});
-        while(!pq.empty()){
-            int node_dist = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
+        st.insert({0,s});
+        while(!st.empty()){
+            auto temp = *(st.begin());
+            int dis = temp.first;
+            int node = temp.second;
+            st.erase(temp);
+            
             for(auto it: adj[node]){
-                // it[0] = adjacent node
-                // it[1] = edgeWeight
-                if(node_dist + it[1]< dist[it[0]]){
-                    dist[it[0]] = node_dist + it[1];
-                    pq.push({dist[it[0]], it[0]});
+                int adjNode = it[0];
+                int edgeWt = it[1];
+                
+                if(dis + edgeWt< dist[adjNode]){
+                    // erase if someone already visited the adjNode
+                    if(dist[adjNode] != 1e9){
+                        st.erase({dist[adjNode],adjNode});
+                    }
+                    dist[adjNode] = dis + edgeWt;
+                    st.insert({dist[adjNode],adjNode});
                 }
+                
             }
         }
         return dist;
