be2ba86b7c36c0f570fd9839dd212365bf3a0f16
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-11-11 11:48:17 +0530

    Time: 276 ms (77.56%), Space: 19.9 MB (92.01%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,46 +1,37 @@
 class Solution {
-    vector<vector<int>> dirs = {
-                {-1,0},
-        {0,-1},         {0,1},
-                {1, 0}
-    };
-    
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        using pipii = pair<int,pair<int,int>>;
-        priority_queue<pipii, vector<pipii>, greater<pipii>> pq;
+        int n = heights.size();
+        int m = heights[0].size();
+        // {diff,{row,col}}
+        priority_queue<pair<int,pair<int,int>>, 
+        vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>pq;
+        pq.push({0,{0,0}});
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
         
-        int ROWS = heights.size();
-        int COLS = heights[0].size();
-        
-        vector<vector<int>> dists(ROWS, vector<int>(COLS, INT_MAX));
-        
-        dists[0][0] = 0;
-        
-        pq.push({0, {0,0}});
-        while(!pq.empty()) {
-            auto node = pq.top();
+        dist[0][0] = 0;
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+        while(!pq.empty()){
+            auto it = pq.top();
             pq.pop();
-            
-            int weight = node.first;
-            auto coords = node.second;
-            int r = coords.first;
-            int c = coords.second;
-            if(dists[r][c] < weight) continue;
-            if(r == ROWS - 1 && c == COLS - 1) return weight;
-            
-            for(auto& dir : dirs) {
-                int dr = r + dir[0];
-                int dc = c + dir[1];
-                if(dr < 0 || dr >= ROWS) continue;
-                if(dc < 0 || dc >= COLS) continue;
-                int newWeight = abs(heights[r][c] - heights[dr][dc]);
-                newWeight = max(newWeight, weight);
-                if(dists[dr][dc] <= newWeight) continue;
-                dists[dr][dc] = newWeight;
-                pq.push({newWeight, {dr, dc}});         
-            }  
-        }     
-        return 0;      
+            int diff = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+            if(row == n-1 && col == m-1) return diff;
+            for(int i=0;i<4;i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m){
+                    //int newdiff = abs(heights[nrow][ncol] - heights[row][col]);
+                    int newEffort = max(abs(heights[nrow][ncol] - heights[row][col]),diff);
+                    if(newEffort<dist[nrow][ncol]){
+                        dist[nrow][ncol] = newEffort;
+                        pq.push({newEffort,{nrow,ncol}});
+                    }
+                }
+            }
+        }
+        return 0;
     }
 };
