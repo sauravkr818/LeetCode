d8f4b58cf4acc9cfbe6bb02990b32150323f481b
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2022-11-18 22:56:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.56%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        //if(n<0) n = -n;
+        
         if(n<=0) return false;
         while (n % 2 == 0)
         {
-            
             n = n/2;
-            //cout<<n<<endl;
         }
         
-        // n must be odd at this point. So we can skip
-        // one element (Note i = i +2)
-        for (int i = 3; i <= sqrt(n); i = i + 2)
+        
+        while(n % 3 == 0)
         {
-            // While i divides n, print i and divide n
-            while (n % i == 0)
-            {
-                //cout << i << " ";
-                if(i != 3 && i!= 5) return false;
-                n = n/i;
-                //cout<<n<<endl;
-            }
+            n = n/3;
         }
-        //cout<<n<<endl;
         
-        if(n>2 && n!=3 && n!=5) return false;
-        return true;
+        while(n%5 == 0)
+        {
+            n = n/5;
+        }
+        return n==1? true: false;
     }
 };
