23d6fbabcb4d26b7907b37d635ec60f2135a32e9
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2023-01-03 18:13:02 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -4,23 +4,26 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    private:
+  public:
+    // Function to find the number of islands.
     
-    void bfs(int row, int col, vector<vector<int>>&vis, vector<vector<char>>&grid){
+    void bfs(int row, int col, vector<vector<int>>&vis, vector<vector<char>>& grid){
         vis[row][col] = 1;
         queue<pair<int,int>>q;
         q.push({row,col});
         int m = grid.size();
         int n = grid[0].size();
-        while(q.empty() == false){
+        vector<int>delrow = {-1,0,1};
+        vector<int>delcol = {-1,0,1};
+        while(!q.empty()){
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
-            for(int deltarow=-1;deltarow<=1;deltarow++){
-                for(int deltacol = -1; deltacol<=1;deltacol++){
-                    int newrow = row + deltarow;
-                    int newcol = col + deltacol;
-                    if(newrow>=0 && newrow<m && newcol>=0 && newcol<n &&
+            for(int i=0;i<delrow.size();i++){
+                for(int j=0;j<delcol.size();j++){
+                    int newrow = row + delrow[i];
+                    int newcol = col + delcol[j];
+                    if(newrow>=0 && newrow<m && newcol>=0 && newcol<n && 
                     !vis[newrow][newcol] && grid[newrow][newcol] == '1'){
                         vis[newrow][newcol] = 1;
                         q.push({newrow,newcol});
@@ -30,16 +33,12 @@ class Solution {
         }
         
     }
-    
-  public:
-    // Function to find the number of islands.
     int numIslands(vector<vector<char>>& grid) {
         // Code here
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>vis(m,vector<int>(n,0));
-        
         int cnt = 0;
+        vector<vector<int>>vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(!vis[i][j] && grid[i][j] == '1'){
@@ -49,7 +48,6 @@ class Solution {
             }
         }
         return cnt;
-        
     }
 };
 
