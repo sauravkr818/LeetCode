4fefd3cee601bc344def74536664ca64018aeea1
Saurav Kumar <71181112+sauravkr818@users.noreply.github.com>
2023-01-05 13:52:42 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,58 +6,28 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    
-    // bfs
-    
-    // bool detectCycle(int src, vector<int>adj[], int vis[]){
-    //     vis[src] = 1;
-    //     queue<pair<int,int>>q; // pair<src,parent where it came from>
-    //     q.push({src,-1});
-    //     while(!q.empty()){
-    //         int adjNode = q.front().first;
-    //         int parent = q.front().second;
-    //         q.pop();
-    //         for(auto it: adj[adjNode]){
-    //             if(!vis[it]){
-    //                 q.push({it,adjNode});
-    //                 vis[it] = 1;
-    //             }
-    //             else if(parent != it) return true;
-    //         }
-    //     }
-    //     return false;
-    // }
-    
-    bool dfs(int node, int parent, vector<int>adj[], int vis[]){
-        vis[node] = 1;
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                if(dfs(it,node,adj,vis)) return true;
-            }
-            else if(it != parent) return true;
-        }
-        return false;
-    }
-    
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
-        int vis[V] = {0};
-        // bfs
-        
-        // connected components
-        // for(int i=0;i<V;i++){
-        //     if(!vis[i]){
-        //         if(detectCycle(i,adj,vis) == true) return true;
-        //     }
-        // }
-        // return false;
-        
-        // dfs
-        
-        // connected components
+        vector<int>vis(V,0);
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(dfs(i,-1,adj,vis) == true) return true;
+                queue<pair<int,int>>q;
+                q.push({i,-1});
+                vis[i] = 1;
+                while(!q.empty()){
+                    int node = q.front().first;
+                    int parent = q.front().second;
+                    q.pop();
+                    for(auto child: adj[node]){
+                        if(!vis[child]){
+                            q.push({child,node});
+                            vis[child] = 1;
+                        }
+                        else if(child != parent){
+                            return true;
+                        }
+                    }
+                }
             }
         }
         return false;
